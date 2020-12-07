faafc660f5f5aeceb9216d7b79a21cd1e4f4e1af
Sergiy Dudnik <sergvstrc@gmail.com>
2020-12-07 08:32:49 +0200

    ver1

--- app/src/main/java/vegfrudai/selomisto/MainActivity.kt
+++ app/src/main/java/vegfrudai/selomisto/MainActivity.kt
@@ -9,17 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvText = findViewById(R.id.tvText)
-        tvText?.setText(number.toString())
+        //tvText = findViewById(R.id.tvText)
+        //tvText?.setText(number.toString())
         CreateSimpleDialog()
 
     }
-    private var number:Int = 0
-    private var tvText: TextView? = null
+    //private var number:Int = 0
+    //private var tvText: TextView? = null
     private fun CreateSimpleDialog()
     {
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Thid is alert dialog")
+        builder.setTitle("Alert dialog")
+        builder.setMessage("This is alert dialog")
+        buolder.setNeutralButton("")
         builder.show()
     }
 }
